fd8d6017461bc2d539095065dca2acfbfec38beb
derekkerns1217 <derekkerns1992@yahoo.com>
2025-11-25 08:05:09 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,19 @@ mkdir -p /root/.op/data
 mkdir -p /root/.op/config
 chmod -R 700 /root/.op
 
-# Start sync first (it needs to be listening)
+# Start sync process
 OP_HTTP_PORT=9090 \
 OP_BUS_PORT=11220 \
+OP_BUS_PEERS=127.0.0.1:11221 \
 /bin/connect-sync &
 
-# Give sync a second to start its bus listener
-sleep 3
+# Give sync time to start
+sleep 5
 
-# Start API and tell it where to find sync's bus
+# Start API process  
 OP_HTTP_PORT=8080 \
 OP_BUS_PORT=11221 \
-OP_BUS_PEERS=localhost:11220 \
+OP_BUS_PEERS=127.0.0.1:11220 \
 /bin/connect-api &
 
 # Wait for either to exit
